e9ce4f4b7352ba6ef08f7635a47db1fbf48b7240
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-08-10 23:10:00 +0530

    added /game landing page

---
+++ src/app/game/page.tsx
@@ -0,0 +1,11 @@
+
+export default function GamePage() {
+  return (
+    <section>
+      <div className="text-center mt-6">
+        <h2 className="md:text-5xl mb-3 font-semibold">Choose Your Learning Track</h2>
+        <p className="max-w-xl mx-auto md:text-lg">Progress through carefully designed learning paths that build your economic knowledge step by step.</p>
+      </div>
+    </section>
+  )
+}
