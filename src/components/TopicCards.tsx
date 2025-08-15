c8d6fac3ec2adcb8a7e1934eee848f7e87dbc4f5
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-08-15 22:48:17 +0530

    added bg color to topic cards

--- src/components/TopicCards.tsx
+++ src/components/TopicCards.tsx
@@ -14,7 +14,7 @@ export const TopicCards = () => {
   return (
     <section className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
       {topics.map((topic) => (
-        <Card key={topic.id} className="hover:-translate-y-1 transition-all duration-500">
+        <Card key={topic.id} className="bg-gray-50 hover:-translate-y-1 transition-all duration-500">
           <CardHeader>
             <topic.icon />
             <CardTitle className="text-xl">{topic.title}</CardTitle>
